67be8c96585668a4eef461f3d2b051e6836b52bc
AbhinavT <athawait5@gmail.com>
2018-03-20 15:40:34 +0530

    initial commit

---
+++ app/src/main/java/picker/android/monthyearpicker/MainActivity.java
@@ -0,0 +1,54 @@
+package picker.android.monthyearpicker;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView click_me;
+    String monthYearStr;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("MMM yyyy");
+    SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click_me = findViewById(R.id.click_me);
+
+        click_me.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MonthYearPickerDialog pickerDialog = new MonthYearPickerDialog();
+                pickerDialog.setListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int i2) {
+                        monthYearStr = year + "-" + (month + 1) + "-" + i2;
+                        click_me.setText(formatMonthYear(monthYearStr));
+                    }
+                });
+                pickerDialog.show(getSupportFragmentManager(), "MonthYearPickerDialog");
+            }
+        });
+    }
+
+    String formatMonthYear(String str) {
+        Date date = null;
+        try {
+            date = input.parse(str);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return sdf.format(date);
+    }
+}
---
+++ app/src/main/java/picker/android/monthyearpicker/MonthYearPickerDialog.java
@@ -0,0 +1,64 @@
+package picker.android.monthyearpicker;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by ankititjunkies on 20/03/18.
+ */
+
+public class MonthYearPickerDialog extends DialogFragment {
+
+    private static final int MAX_YEAR = 2099;
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        Calendar cal = Calendar.getInstance();
+
+        View dialog = inflater.inflate(R.layout.month_year_picker_dialog, null);
+        final NumberPicker monthPicker = (NumberPicker) dialog.findViewById(R.id.picker_month);
+        final NumberPicker yearPicker = (NumberPicker) dialog.findViewById(R.id.picker_year);
+
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(cal.get(Calendar.MONTH) + 1);
+
+        int year = cal.get(Calendar.YEAR);
+        yearPicker.setMinValue(2000);
+        yearPicker.setMaxValue(year);
+        yearPicker.setValue(year);
+
+        builder.setView(dialog)
+                // Add action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDateSet(null, yearPicker.getValue(), monthPicker.getValue(), 0);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MonthYearPickerDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
